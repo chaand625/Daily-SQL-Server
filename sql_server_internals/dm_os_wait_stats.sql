bcb89337c18a21f03248d9563ac868491b90bda1
bashac625 <bashac625@gmail.com>
2026-07-20 22:29:34 +0530

    added dm_os_wait_stats.sql to measure the signal wait time and resrouce wait time.

---
+++ sql_server_internals/dm_os_wait_stats.sql
@@ -0,0 +1,11 @@
+SELECT
+    SUM(signal_wait_time_ms) AS [Signal Wait Time (ms)]
+    , CONVERT(DECIMAL(7,4), 100.0 * SUM (signal_wait_time_ms) / SUM(wait_time_ms)) AS [% Signal waits]
+    , SUM(wait_time_ms - signal_wait_time_ms) AS [Resource Wait Time (ms)]
+    , CONVERT (DECIMAL(7,4), 100.0 * sum(wait_time_ms - signal_wait_time_ms) / SUM(wait_time_ms)) AS [% Resource waits]
+FROM
+    sys.dm_os_wait_stats WITH (NOLOCK);
+
+/*
+    Statistics are collected from the time of the last SQL Server restart.
+*/
